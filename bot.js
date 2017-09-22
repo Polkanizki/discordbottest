74dd9d1a750f484ebbf5ca889b6985a87df51bf1
Polkanizki <cesario.angelino@gmail.com>
2017-09-22 15:07:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,14 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-client.login('process.env.BOT_TOKEN');
 
-client.on('ready', message => {
-  console.log(`Bot attivo su ${client.user.tag}`);
+client.on('ready', () => {
+  console.log(`Loggato su ${client.user.tag}!`);
 });
 
 client.on('message', message => {
-  if(message.content === 'ping') {
-    message.channel.send('pong');
+  if (message.content === 'ping') {
+    message.reply('Pong!');
   }
 });
+
+client.login('process.env.BOT_TOKEN');
