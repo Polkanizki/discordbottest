e9170bdfaa994ab1b56ed857bc7cdc960ac7e097
Polkanizki <cesario.angelino@gmail.com>
2017-09-23 22:56:40 +0200

    Other

--- bot.js
+++ bot.js
@@ -15,8 +15,7 @@ var admin = '187657725407264768';
 client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => {
-    console.log(`Il bot e' stato avviato, con ${client.users.size} utenti, in ${client.channels.size} canali di ${client.guilds.size} server.`);
-	console.log(`Loggato su ${client.user.tag}!`);
+    console.log(`Il bot e' stato avviato su ${client.user.tag}!, con ${client.users.size} utenti, in ${client.channels.size} canali di ${client.guilds.size} server.`);
 	client.user.setPresence({ game: {
 		name: `${client.guilds.size} server ~Polkanizki`, type: 0
 	}});
