17f725c6d7fdd7a1b444547a0505ef7aead3c9d9
Polkanizki <cesario.angelino@gmail.com>
2019-04-21 21:32:29 +0200

    Ripristino vecchia funzione

--- bot.js
+++ bot.js
@@ -15,7 +15,9 @@ var admin = '187657725407264768';
 
 client.on('ready', () => {
     console.log(`Il bot e' stato avviato su ${client.user.tag}!, con ${client.users.size} utenti, in ${client.channels.size} canali di ${client.guilds.size} server.`);
-	client.user.setActivity(`${client.guilds.size} server | https://discord.gg/DEGhsTh | ~Polkanizki`);
+	client.user.setPresence({ game: {
+		name: `${client.guilds.size} server | https://discord.gg/DEGhsTh | ~Polkanizki`, type: 0
+	}});
 });
 
 client.on('reconnecting', () => {
